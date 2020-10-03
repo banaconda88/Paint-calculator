9a7636906c91b633d4e5029547ce978c929026b3
banaconda88 <71533657+banaconda88@users.noreply.github.com>
2020-10-03 19:47:19 -0400

    Add files via upload

---
+++ Paint calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+L = eval(input("What is the length of the room you would like to paint in feet?"))
+W = eval(input("What is the width of the room you would like to paint in feet?"))
+H = eval(input("What is the height of the room you would like to paint in feet?"))
+D = eval(input("How many doors are in the room you would like to paint?"))
+wi = eval(input("How many windows are in the room you would like to paint?"))
+A = L * W + L * H + H * W
+a = 2 * A
+T = a - D * 20 - wi * 15
+G = T / 350
+V = "You will need to cover {} square feet."
+v = "You will need {} gallons of paint to paint this room."
+print(V.format(T))
+print(v.format(G))
+print("You're welcome :).")
+
